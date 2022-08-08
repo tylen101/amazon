d5be7b6c9b0bf7bcffc35f5a4b0e4bb4b738c3fb
tylen101 <tylenwcarroll@gmail.com>
2022-08-08 15:47:49 -0400

    updated gitignore to leave out package-lock.json

--- src/components/CheckoutProduct.js
+++ src/components/CheckoutProduct.js
@@ -16,9 +16,9 @@ function CheckoutProduct({ id, image, title, price, rating }) {
       <img className="checkoutProduct__image" src={image} alt="" />
       <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
-        <p className="checkoutProduct__price">{price}</p>
-        <small>$</small>
-        <strong>{price}</strong>
+        <p className="checkoutProduct__price"><small>$</small>
+        <strong>{price}</strong></p>
+
         <div className="checkoutProduct__rating">
           {Array(rating)
             .fill()
--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import '../CSS/subtotal.css';
-import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from '../store/StateProvider';
 import { getBasketTotal } from '../store/reducer';
 export default function Subtotal() {
   const [{ basket }, dispatch] = useStateValue();
+  const value = getBasketTotal(basket);
   return (
     <div className="subtotal">
-      <CurrencyFormat
+      {/* <CurrencyFormat
         renderText={(value) => (
-          <>
-            <p>
-              Subtotal ({basket.length} items):
-              <strong>{value}</strong>
-            </p>
-            <small className="subtotal__gift">
-              <input type="checkbox" />
-              This order contains a gift
-            </small>
-          </>
-        )}
+          // <> */}
+      <p>
+        Subtotal ({basket.length} items):
+        <strong>
+          {new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+          }).format(value)}{' '}
+        </strong>
+      </p>
+      <small className="subtotal__gift">
+        <input type="checkbox" />
+        This order contains a gift
+      </small>
+      {/* </> */}
+      {/* )}
         decimalScale={2}
         value={getBasketTotal(basket)}
         displayType={'text'}
         thousandSeparator={true}
         prefix={'$'}
-      />
+      /> */}
       <button>Proceed to Checkout</button>
     </div>
   );
